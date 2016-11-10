73eb47f1656a548a55955775cac60572c22760ea
xl2630 <Xinyi@liuxinyis-MacBook.local>
2016-11-09 22:10:48 -0500

    SUBMIT MIDTERM

---
+++ HTML MDT/code.js
@@ -0,0 +1,16 @@
+document.getElementById('submit1').addEventListener('click',function(){alert(" Submitted! You got the right one!");});
+document.getElementById('submit2').addEventListener('click',function(){alert(" Submitted! You got the right one!");});
+document.getElementById('submit3').addEventListener('click',function(){alert(" Submitted! You got the right one!");});
+
+// Pseudocode 
+
+// line 0 → /* Functionality for single quiz unit
+// line 1 → user clicks on the circle to submit answer
+// line 2 → system checks answer
+// line 3 → if answer is correct
+// line 4 →     system alerts user with congregation message
+// line 5 →if answer is wrong
+// line 6 →     system marks wrong answer in red
+// line 7 →     system alerts user with error message
+// line 8 → restore results data to the database
+// line 9 → repeat the process for the rest of the quizzes */
